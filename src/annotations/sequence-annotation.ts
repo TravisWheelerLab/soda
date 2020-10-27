37598e8ec72b120e1668c9bbdf945d2e54ccb120
jackroddy <jack.w.roddy@gmail.com>
2020-10-27 11:36:39 -0600

    Sequence annotation works, but it's slow
    I'll come back to this later when I have more time. The issue is that
    each character is its own svg text element. It was really easy to
    implement this way, but I think I will mess around with calculating the
    size of monospace fonts in the future.

--- src/annotations/sequence-annotation.ts
+++ src/annotations/sequence-annotation.ts
@@ -1,21 +1,21 @@
 import {Annotation} from "./annotation";
 import {SequenceAnnotationConfig} from "./sequence-annotation-config";
 
-interface characterDatum {
+export interface CharacterDatum {
     char: string;
     x: number;
 }
 
 export class SequenceAnnotation extends Annotation {
     sequence: string;
-    characters: characterDatum[] = [];
+    characters: CharacterDatum[] = [];
 
     public constructor(conf: SequenceAnnotationConfig) {
         super(conf);
         this.sequence = conf.sequence;
         let i = 0;
         for (const c of this.sequence) {
-            this.characters.push({char: c, x: i++})
+            this.characters.push({char: c, x: (this.x + i++)})
         }
     }
 }
--- src/primitives/sequence/sequence.ts
+++ src/primitives/sequence/sequence.ts
@@ -1,11 +1,14 @@
-import {SequenceAnnotation} from "../../annotations/sequence-annotation";
+import {CharacterDatum, SequenceAnnotation} from "../../annotations/sequence-annotation";
 import {Chart} from "../../charts/chart";
 import {SequenceConfig} from "./sequence-config";
+import * as defaults from "./sequence-defaults";
+import {isZoomableChart} from "../../plugins/zoom/zoomable-chart";
+import {registerZoomBehavior} from "../../primitives";
 
-export function sequence<A extends SequenceAnnotation, C extends Chart<any>>(chart: C, ann: A[], conf: SequenceConfig<A, C>) {
-    let x: (a: A, c: C) => number = conf.x;
-    let w: (a: A, c: C) => number = conf.w;
-    let y: (a: A, c: C) => number = conf.y;
+export function sequence<A extends SequenceAnnotation, D extends CharacterDatum, C extends Chart<any>>(chart: C, ann: A[], conf: SequenceConfig<A, C>) {
+    let x: (a: A, c: C) => number = conf.x || defaults.sequenceXFn;
+    // let w: (a: A, c: C) => number = conf.w;
+    let y: (a: A, c: C) => number = conf.y || defaults.sequenceYFn ;
 
     const selection = chart.svgSelection
         .selectAll<SVGGElement, A>(`g.${conf.selector}`)
@@ -13,7 +16,9 @@ export function sequence<A extends SequenceAnnotation, C extends Chart<any>>(cha
 
     const enter = selection.enter()
         .append('g')
-        .attr('transform', (a) => `translate(${chart.getXScale()(x(a, chart))},${y(a, chart)})`);
+        .attr('class', conf.selector)
+        // .attr('transform', (a) => `translate(${chart.getXScale()(x(a, chart))},${y(a, chart)})`);
+        .attr('transform', (a) => `translate(0,${y(a, chart)})`);
 
     const merge = enter.merge(selection);
 
@@ -22,14 +27,11 @@ export function sequence<A extends SequenceAnnotation, C extends Chart<any>>(cha
         .data((a) => a.characters)
         .enter()
             .append('text')
-            // .attr('fill', 'black')
+            .attr('class', conf.selector)
             .text((c) => c.char)
             .attr('y', 0)
-            // .attr('y', (c) => c.y * chart.binHeight)
-            .attr('x', (c) => chart.getXScale()(c.x))
-            .style('text-anchor', 'middle')
-            // .attr('height', chart.binHeight)
-            // .attr('width', chart.getXScale()(1));
+            .attr('x', (d) => chart.getXScale()(d.x))
+            .style('text-anchor', 'middle');
 
     // set the position parameters
     merge
@@ -40,4 +42,9 @@ export function sequence<A extends SequenceAnnotation, C extends Chart<any>>(cha
     selection.exit()
         .remove();
 
+    if (isZoomableChart(chart)) {
+        // if the chart is zoomable, register the ZoomBehavior for the text
+        // registerZoomBehavior(chart, new defaults.SequenceZoomBehavior(conf.selector, x, y));
+        registerZoomBehavior(chart, new defaults.SequenceCharacterZoomBehavior(conf.selector));
+    }
 }
