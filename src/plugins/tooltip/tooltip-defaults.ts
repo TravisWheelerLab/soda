4db9b112494c6e6c7fc613f59f619b1e9d1cc2ae
jackroddy <jack.w.roddy@gmail.com>
2020-11-02 14:41:43 -0700

    Tooltips are now more configurable.
    Now the tooltip hover callbacks have the same configuration pattern that
    the glyph rendering has. This means that a tooltip can be dynamically
    configured with any information from an Annotation or the Chart it's
    rendered in.

---
+++ src/plugins/tooltip/tooltip-defaults.ts
@@ -0,0 +1,7 @@
+export const tooltipOpacity = () => 1.0;
+export const tooltipBackgroundColor = () => 'lightsteelblue';
+export const tooltipTextColor = () => 'black';
+export const tooltipBorderRadius = () => 8;
+export const tooltipPadding = () => 4;
+
+
