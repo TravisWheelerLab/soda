787d6c71aa09968bb8943be4a449dd6ea93a57b3
jackroddy <jack.w.roddy@gmail.com>
2020-12-11 14:58:04 -0700

    changed the ip

--- tests/scripts/generate-comparefiles.ts
+++ tests/scripts/generate-comparefiles.ts
@@ -1,7 +1,7 @@
 import * as pf from "./puppeteer-functions";
 
 (async () => {
-    await pf.saveTrackChartSvgXml('http://[::1]:8080/rectangles/', '../tmp/', 'rectangles');
-    await pf.saveTrackChartSvgXml('http://[::1]:8080/lines/', '../tmp/', 'lines');
+    await pf.saveTrackChartSvgXml('http://http://127.0.0.1:8080/rectangles/', '../tmp/', 'rectangles');
+    await pf.saveTrackChartSvgXml('http://http://127.0.0.1:8080/lines/', '../tmp/', 'lines');
 })();
 
