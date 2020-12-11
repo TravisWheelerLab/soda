1641537325fe062693a7eaac959b92ef7d81ee45
jackroddy <jack.w.roddy@gmail.com>
2020-12-11 14:14:29 -0700

    Added a simple testing framework

--- tests/scripts/generate-goldfiles.ts
+++ tests/scripts/generate-goldfiles.ts
@@ -1,31 +1,9 @@
 // import {strict as assert} from 'assert';
-import * as puppeteer from 'puppeteer';
-import * as fs from 'fs';
+import * as pf from './puppeteer-functions'
 
 (async () => {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-
-    await Promise.all([
-        page.goto('http://[::1]:8080/rectangles/index.html'),
-        page.waitForNavigation(),
-    ]);
-
-    let svgInline= await page.evaluate(() => document.querySelector('div#track-chart>svg')!.outerHTML);
-    fs.writeFile('chart-initial.svg', svgInline, ()=> console.log('writ') );
-
-    await page.hover('div#track-chart>svg');
-    await page.keyboard.down('Control');
-    // @ts-ignore
-    await page.mouse.wheel({ deltaY: -1000 });
-
-    svgInline= await page.evaluate(() => document.querySelector('div#track-chart>svg')!.outerHTML);
-    fs.writeFile('chart-zoomin.svg', svgInline, ()=> console.log('writ') );
-
-    // @ts-ignore
-    await page.mouse.wheel({ deltaY: 1000 });
-    svgInline= await page.evaluate(() => document.querySelector('div#track-chart>svg')!.outerHTML);
-    fs.writeFile('chart-zoomback.svg', svgInline, ()=> console.log('writ') );
-
-    // await browser.close();
+    await pf.saveTrackChartSvgXml('http://[::1]:8080/rectangles/', '../goldfiles/', 'rectangles');
+    await pf.saveTrackChartSvgXml('http://[::1]:8080/lines/', '../goldfiles/', 'lines');
 })();
+
+
---
+++ tests/scripts/puppeteer-functions.ts
@@ -0,0 +1,61 @@
+import * as puppeteer from "puppeteer";
+import * as fs from "fs";
+import {installMouseHelper} from "./install-mouse-helper";
+
+export async function saveTrackChartSvgXml(url: string, outpath: string, outname: string) {
+    let pageX: number = 0;
+    let pageY: number = 0;
+
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+
+    await installMouseHelper(page);
+
+    await Promise.all([
+        page.goto(url),
+        page.waitForNavigation(),
+    ]);
+
+    let svgInline = await page.evaluate(() => document.querySelector('div#track-chart>svg')!.outerHTML);
+    fs.writeFile(`${outpath}/${outname}-initial.svg`, svgInline, ()=> console.log(`${outpath}/${outname}-initial.svg written`) );
+
+    await page.hover('div#track-chart>svg');
+    await page.keyboard.down('Control');
+    await page.mouse.wheel({ deltaY: -1000 });
+
+    svgInline= await page.evaluate(() => document.querySelector('div#track-chart>svg')!.outerHTML);
+    fs.writeFile(`${outpath}/${outname}-zoomed-in.svg`, svgInline, ()=> console.log(`${outpath}/${outname}-zoomed-in.svg written`) );
+
+    await page.mouse.wheel({ deltaY: 1000 });
+
+    svgInline= await page.evaluate(() => document.querySelector('div#track-chart>svg')!.outerHTML);
+    fs.writeFile(`${outpath}/${outname}-initial2.svg`, svgInline, ()=> console.log(`${outpath}/${outname}-initial2.svg written`) );
+
+    await page.mouse.wheel({ deltaY: 1000 });
+
+    svgInline= await page.evaluate(() => document.querySelector('div#track-chart>svg')!.outerHTML);
+    fs.writeFile(`${outpath}/${outname}-zoomed-out.svg`, svgInline, ()=> console.log(`${outpath}/${outname}-zoomed-out.svg written`) );
+
+    await page.mouse.wheel({ deltaY: -1000 });
+
+    svgInline= await page.evaluate(() => document.querySelector('div#track-chart>svg')!.outerHTML);
+    fs.writeFile(`${outpath}/${outname}-initial3.svg`, svgInline, ()=> console.log(`${outpath}/${outname}-initial3.svg written`) );
+
+    await page.keyboard.up('Control');
+
+    await page.mouse.move(70, 70);
+    await page.mouse.down();
+    await page.mouse.move(500, 70);
+
+    svgInline= await page.evaluate(() => document.querySelector('div#track-chart>svg')!.outerHTML);
+    fs.writeFile(`${outpath}/${outname}-scrolled.svg`, svgInline, ()=> console.log(`${outpath}/${outname}-scrolled.svg written`) );
+
+    await page.mouse.move(70, 70);
+
+    svgInline= await page.evaluate(() => document.querySelector('div#track-chart>svg')!.outerHTML);
+    fs.writeFile(`${outpath}/${outname}-initial4.svg`, svgInline, ()=> console.log(`${outpath}/${outname}-initial4.svg written`) );
+
+    await page.mouse.up();
+
+    await browser.close();
+}
