64b88b4a0617cfd7ecfd764a41d40517002cdc12
Jack Roddy <jack.w.roddy@gmail.com>
2021-01-26 21:54:15 -0700

    AnnotationGroup now has a type param
    It just makes sense for an AnnotationGroup to typed with the Annotation
    class that it holds.

--- src/annotations/annotation-group.ts
+++ src/annotations/annotation-group.ts
@@ -4,9 +4,10 @@ import {Annotation, AnnotationConfig} from "./annotation";
  * An Annotation class that contains a group of Annotations. Mostly, this is used to maintain the group of
  * Annotations at the same vertical position--all Annotations in the group will be set to the same y coordinate
  * when the setY() method is called on the AnnotationGroup.
+ * @typeParam A The type of annotation that will live in this group.
  */
-export class AnnotationGroup extends Annotation {
-    group: Annotation[] = [];
+export class AnnotationGroup<A extends Annotation> extends Annotation {
+    group: A[] = [];
     x2: number;
 
     constructor(conf: AnnotationConfig) {
@@ -14,7 +15,7 @@ export class AnnotationGroup extends Annotation {
        this.x2 = conf.x + conf.w;
     }
 
-    public add(ann: Annotation) {
+    public add(ann: A) {
         if (this.x > ann.x) {
             this.x = ann.x;
         }
