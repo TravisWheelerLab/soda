8612bcb6de731e1c34d129afcb4b4b9946045afa
Jack Roddy <jack.w.roddy@gmail.com>
2021-03-14 18:09:14 -0600

    Updated test symlinks

--- tests/test-apps/init.sh
+++ tests/test-apps/init.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 cp webpack.config-test.js $1/webpack.config.js
-ln -sf ./test-makefile $1/Makefile
-ln -sf ./tsconfig-test.json $1/tsconfig.json
-ln -sf ./ann.ts $1
+cd $1
+ln -sf ../test-makefile Makefile
+ln -sf ../tsconfig-test.json tsconfig.json
