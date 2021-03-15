ed06b7e3a4e34f066309e9bde1445bc46be6c2a3
Jack Roddy <jack.w.roddy@gmail.com>
2021-03-14 18:02:45 -0600

    Updated symlinks

--- examples/init.sh
+++ examples/init.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 cp webpack.config-example.js $1/webpack.config.js
-ln -sf $PWD/example-makefile $1/Makefile
-ln -sf $PWD/tsconfig-example.json $1/tsconfig.json
+cd $1
+ln -sf ../example-makefile Makefile
+ln -sf ../tsconfig-example.json tsconfig.json
--- tests/test-apps/init.sh
+++ tests/test-apps/init.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 cp webpack.config-test.js $1/webpack.config.js
-ln -sf $PWD/test-makefile $1/Makefile
-ln -sf $PWD/tsconfig-test.json $1/tsconfig.json
-ln -sf $PWD/ann.ts $1
+ln -sf ./test-makefile $1/Makefile
+ln -sf ./tsconfig-test.json $1/tsconfig.json
+ln -sf ./ann.ts $1
